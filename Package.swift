6edf09cd1fb814b13f603d8e706347f88e56259f
Mansi Barodia <49169550+mansibarodia10057@users.noreply.github.com>
2024-10-01 18:19:37 -0700

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MSGraphClientModels",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MSGraphClientModels",
+            targets: ["MSGraphClientModels"]),
+    ],
+    targets: [
+        .binaryTarget(name: "MSGraphClientModels", url: "https://github.com/mansibarodia10057/msgraph-sdk-objc-models/releases/download/1.3.0-spm/MSGraphClientModels.xcframework.zip", checksum: "f3c9250990a11a35faedd1239bc9107d637c1dc1e5ede50990fda98b927b0f31")
+
+    ]
+)
